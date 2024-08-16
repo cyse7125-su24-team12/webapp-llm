0a29387c63858b5df69455842bcbe0f2598d4172
Balasubramanian U <ubalasubramanian03@gmail.com>
2024-08-16 16:18:56 -0400

    feat: added ollama changes

--- app/model.py
+++ app/model.py
@@ -1,33 +1,31 @@
-# Import necessary libraries and load your model here
-
-
-from langchain_groq import ChatGroq
+import os
+from . import create_json_logger
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
 from langchain.embeddings import HuggingFaceHubEmbeddings
-from langchain_huggingface import HuggingFaceEndpointEmbeddings
-import os
-from . import create_json_logger
+from langchain_community.llms.ollama import Ollama
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 # Initialize logger
 logger = create_json_logger()
 
 index_name = os.getenv("PINECONE_INDEX_NAME")
 namespace = os.getenv("PINECONE_NAMESPACE")
-model_name = os.getenv("GROQ_MODEL_NAME")
+pinecone_api_key = os.getenv("PINECONE_API_KEY")
+hface_api_token = os.getenv("HFACE_API_TOKEN")
+ollama_url = os.getenv("OLLAMA_URL")
 # print(os.getenv("GROQ_API_KEY"))
 # print(os.getenv("PINECONE_API_KEY"))
 # print(os.getenv("HFACE_API_TOKEN"))
-os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
-os.environ["PINECONE_API_KEY"] = os.getenv("PINECONE_API_KEY")
-pinecone = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+pinecone = Pinecone(api_key=pinecone_api_key)
 
 # Set up the embedding model to use via Hugging Face Inference API
 embeddings = HuggingFaceHubEmbeddings(
-    repo_id="sentence-transformers/all-MiniLM-L6-v2",  # Public model name on Hugging Face
-    huggingfacehub_api_token=os.getenv("HFACE_API_TOKEN")  # Your Hugging Face API token
+    repo_id="sentence-transformers/all-MiniLM-L6-v2",
+    huggingfacehub_api_token=hface_api_token
 )
 
 # Create an index if it does not exist
@@ -39,14 +37,14 @@ db = PineconeVectorStore(
     namespace=namespace,
     embedding=embeddings
 )
+retriever = db.as_retriever()
+retriever.search_kwargs = {"k": 5} 
 
-llm = ChatGroq(
-    model=model_name,
-    temperature=0,
-    max_tokens=None,
-    timeout=None,
-    max_retries=2,
-    api_key=os.getenv("GROQ_API_KEY")
+model = Ollama(
+    base_url=ollama_url, 
+    model = "llama3:8b",
+    temperature = 0,
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 )
 
 template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
@@ -55,12 +53,9 @@ Question: {question}
 Helpful Answer:"""
 QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
 
-retriever = db.as_retriever()
-# retriever.search_kwargs = {"k": 5} 
-
 # Create the RetrievalQA chain
 qa_chain = RetrievalQA.from_chain_type(
-    llm=llm,
+    llm=model,
     chain_type="stuff",
     retriever=retriever,
     chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
@@ -69,7 +64,9 @@ qa_chain = RetrievalQA.from_chain_type(
 def generate_response(prompt):
     logger.info(f"Generating response for prompt: {prompt}")
     try:
-        response = qa_chain.invoke(prompt)
+        response = ''
+        for chunk in qa_chain.stream("what is the cve_id of SINEC NMS vulnerability?"):
+            response += chunk
         logger.info(f"Generated response: {response}")
     except Exception as e:
         logger.error(f"Error generating response: {e}")
